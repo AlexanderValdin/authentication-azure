fb36165fcfdc67a827de7eced3fa743c56fd260b
Alexander Valenzuela <alvalenzuela2304@gmail.com>
2022-11-19 01:31:28 -0300

    Composition API with setup

--- src/views/AzureView.vue
+++ src/views/AzureView.vue
@@ -1,36 +1,27 @@
 <template>
     <h1>Azure Login</h1>
 </template>
-<script lang="ts">
+<script lang="ts" setup>
     import { PublicClientApplication} from '@azure/msal-browser'
-    import { defineComponent, onMounted } from 'vue'
+    import {onMounted } from 'vue'
     import AzureService from '@/services/AzureService'
-    export default defineComponent({
-        name: 'AzureView',
-        
-        setup() {
-            onMounted(() => {
-                const azureService = new AzureService()
-                $msalInstance = new PublicClientApplication(azureService.getMsalConfig().value)
-            })
-            let account = ''
-
-            const login = async () => {
-                await $msalInstance
-                .loginPopup({})
-                .then( () => {
-                    const myAccounts = $msalInstance.getAllAccounts()
-                    account = myAccounts[0]
-                    $emitter.emit('login', account)
-                })
-                .catch( error => {
-                    alert(error)
-                })
-            }
-
-            return {
-                account, login
-            }
-        }
+    
+    onMounted(() => {
+        const azureService = new AzureService()
+        $msalInstance = new PublicClientApplication(azureService.getMsalConfig().value)
     })
+    let account = ''
+
+    const login = async () => {
+        await $msalInstance
+            .loginPopup({})
+            .then( () => {
+                const myAccounts = $msalInstance.getAllAccounts()
+                account = myAccounts[0]
+                $emitter.emit('login', account)
+            })
+            .catch( error => {
+                alert(error)
+            })
+    }
 </script>
