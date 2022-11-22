11e2501ecf230efc366c7f60626cbb964187ce46
Alexander Valenzuela <alvalenzuela2304@gmail.com>
2022-11-22 13:15:56 -0300

    corrección tipado datos, retorno a Option API por problemas con globalproperties

--- src/services/AzureService.ts
+++ src/services/AzureService.ts
@@ -2,14 +2,14 @@ import {Ref, ref} from 'vue'
 import IMsalConfig from '@/interfaces/IMsalConfig'
 
 class AzureService {
-    private msalConfig:Ref<>
+    private msalConfig:Ref<IMsalConfig>
     private accessToken:Ref<string>
 
     constructor() {
         this.msalConfig = ref({
             auth: {
-                clientId: 'XXXXXX',
-                authority: 'XXXXXX'
+                clientId: 'f827c248-ff1f-4892-adf3-63475debad36',
+                authority: 'https://login.microsoftonline.com/e4c36323-f55d-4975-8c6d-e0a888432631'
             },
             cache: {
                 cacheLocation: 'localStorage'
@@ -26,7 +26,7 @@ class AzureService {
         return this.accessToken
     }
 
-    getMsalConfig():Ref<> {
+    getMsalConfig():Ref<IMsalConfig> {
         return this.msalConfig
     }
 }
--- src/views/AzureView.vue
+++ src/views/AzureView.vue
@@ -1,27 +1,36 @@
 <template>
     <h1>Azure Login</h1>
+    <button @click="login">Login</button>
 </template>
-<script lang="ts" setup>
+<script lang="ts">
     import { PublicClientApplication} from '@azure/msal-browser'
-    import {onMounted } from 'vue'
+    import {defineComponent} from 'vue'
     import AzureService from '@/services/AzureService'
-    
-    onMounted(() => {
-        const azureService = new AzureService()
-        $msalInstance = new PublicClientApplication(azureService.getMsalConfig().value)
-    })
-    let account = ''
 
-    const login = async () => {
-        await $msalInstance
-            .loginPopup({})
-            .then( () => {
-                const myAccounts = $msalInstance.getAllAccounts()
-                account = myAccounts[0]
-                $emitter.emit('login', account)
-            })
-            .catch( error => {
-                alert(error)
-            })
-    }
+    export default defineComponent({
+        name: 'AzureView',
+        data() {
+            return {
+                account: ''
+            }
+        },
+        async created() {
+            const azureService = new AzureService()
+            this.$msalInstance = new PublicClientApplication(azureService.getMsalConfig().value)
+        },
+        methods: {
+            async login() {
+                await this.$msalInstance
+                .loginPopup({})
+                .then( () => {
+                    const myAccounts = this.$msalInstance.getAllAccounts()
+                    this.account = myAccounts[0]
+                    this.$emitter.emit('login',this.account)
+                })
+                .catch( error => {
+                    alert(error)
+                })
+            }
+        }
+    })
 </script>
